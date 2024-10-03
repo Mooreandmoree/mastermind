999b34912cbb8853f008bd16f74de825f001272f
Mooreandmoree <131541515+Mooreandmoree@users.noreply.github.com>
2024-10-03 13:36:52 +0100

    modified:   colorcode.rb

--- colorcode.rb
+++ colorcode.rb
@@ -1,17 +1,16 @@
 class ColorCode
   def initialize(color1=random, color2=random, color3=random, color4=random)
-    @colors = [color1, color, color3, color4]
+    @colors = [color1, color2, color3, color4]
   end
-
-  def random 
-    color_spectrum = ["red", "orange", "yellow", "green", "blue", "indigo", "violet", "white"]
+ 
+  def random
+    color_spectrum = ["red", "orange", "yellow", "green", "blue","indigo", "violet", "white"]
     random = color_spectrum.sample
-    @selected_positons ||= []
-    until !@selected_positons.include?(random)
-      random = color_spectrum.sample
+    @selected_positions ||= []
+    until !@selected_positions.include?(random)
+    random = color_spectrum.sample
     end
-    @selected_positons << random
-    random
+    @selected_positions << random
+    random 
   end
-  
 end
