5b5d77d918aba07ee2216d539abef780aee2f1da
Mooreandmoree <131541515+Mooreandmoree@users.noreply.github.com>
2024-10-08 12:06:48 +0100

    modified:   Gemfile 	modified:   colorcode.rb 	modified:   computer.rb 	modified:   mastermind.rb 	modified:   spec/spec_helper.rb

--- colorcode.rb
+++ colorcode.rb
@@ -1,16 +1,16 @@
+# frozen_string_literal: true
+
 class ColorCode
-  def initialize(color1=random, color2=random, color3=random, color4=random)
+  def initialize(color1 = random, color2 = random, color3 = random, color4 = random)
     @colors = [color1, color2, color3, color4]
   end
- 
+
   def random
-    color_spectrum = ["red", "orange", "yellow", "green", "blue","indigo", "violet", "white"]
+    color_spectrum = %w[red orange yellow green blue indigo violet white]
     random = color_spectrum.sample
     @selected_positions ||= []
-    until !@selected_positions.include?(random)
-    random = color_spectrum.sample
-    end
+    random = color_spectrum.sample while @selected_positions.include?(random)
     @selected_positions << random
-    random 
+    random
   end
 end
